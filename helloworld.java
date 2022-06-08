b22affe81b93944cdaddd05fa783d5d19ac44bac
aparnadevrepo <106479132+aparnadevrepo@users.noreply.github.com>
2022-06-08 08:34:44 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 8th june 8.23 A.M - webhooks demo1!");
+    System.out.println("Hello World 8th june 8.23 A.M - webhooks demo2!");
     }
 }
